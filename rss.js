43725634e0593d6311ccf4ec659f1bb14e222e55
Dali <103475660+zigomar007@users.noreply.github.com>
2022-09-28 18:33:01 +0100

    Update rss.js

--- rss.js
+++ rss.js
@@ -1,6 +1,6 @@
 const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser())
 /* Fetch URLs from JSON */
-fetch('http://cors-anywhere.com/https://www.hongkiat.com', { mode: 'no-cors'}).then((res) => {
+fetch('https://maze.fr', { mode: 'no-cors'}).then((res) => {
 	res.text().then((data) => {
 		var frag = document.createDocumentFragment()
 		var hasBegun = true
